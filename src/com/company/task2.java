559e02998379a3160595f7ad107be8cf133a553e
Artem Chumachenko <artem472216@yandex.ru>
2024-05-10 15:43:34 +0300

    Add all answers

---
+++ src/com/company/task2.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class task2 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("a: ");
+        String a = scanner.next();
+        System.out.print("b: ");
+        String b = scanner.next();
+
+        if(a.equals(b)) System.out.println("Строки идентичны");
+        else System.out.println("Строки неидентичны");
+    }
+}
---
+++ src/com/company/task3.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class task3 {
+    public static void main(String[] args) {
+        int[] a = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        for(int i = 0; i < a.length; i++){
+            if(a[i]%2 == 0)System.out.printf("%d ", a[i]);
+        }
+    }
+}
